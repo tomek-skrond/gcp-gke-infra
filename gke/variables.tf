e9ac432a222fa28b00fe4f02b66bb02a98b197f1
tomek-skrond <tomek2.skrond@gmail.com>
2024-04-27 01:47:31 +0200

    added backend state code

---
+++ gke/variables.tf
@@ -0,0 +1,45 @@
+variable "cloudflare_api_token" {
+  type      = string
+  sensitive = true
+}
+variable "cloudflare_zone_id" {
+  type      = string
+  sensitive = true
+}
+variable "domain_name" {
+  type      = string
+  sensitive = true
+}
+
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+}
+
+variable "cluster_name_suffix" {
+  description = "A suffix to append to the default cluster name"
+  default     = ""
+}
+
+variable "region" {
+  description = "The region to host the cluster in"
+}
+
+variable "network" {
+  description = "The VPC network to host the cluster in"
+}
+
+variable "subnetwork" {
+  description = "The subnetwork to host the cluster in"
+}
+
+variable "ip_range_pods" {
+  description = "The secondary ip range to use for pods"
+}
+
+variable "ip_range_services" {
+  description = "The secondary ip range to use for services"
+}
+
+variable "compute_engine_service_account" {
+  description = "Service account to associate to the nodes in the cluster"
+}
