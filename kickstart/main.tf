9b8d38a6bd5314809e6c9daf49d7c9356483cef7
tomek-skrond <tomek2.skrond@gmail.com>
2024-05-06 14:06:38 +0200

    removed lock

--- kickstart/main.tf
+++ kickstart/main.tf
@@ -5,14 +5,14 @@ provider "google" {
   # zone        = var.zone
 }
 
-#resource "google_project_service" "project" {
-#  project = var.project_id
-#  service = "compute.googleapis.com"
-#
-#  timeouts {
-#    create = "30m"
-#    update = "40m"
-#  }
-#
-#  disable_dependent_services = true
-#}
+resource "google_project_service" "project" {
+  project = var.project_id
+  service = "compute.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
